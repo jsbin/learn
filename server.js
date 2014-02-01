a7b7596eead2cf0722edddeecd99e68bb4b401dc
Remy Sharp <remy@remysharp.com>
2014-02-01 23:30:18 +0000

    now serving static content

--- server.js
+++ server.js
@@ -1,2 +1,15 @@
 'use strict';
-require('harp').server(__dirname, { port: process.env.PORT || 9000 });
+var harp = require('harp');
+var server = require('harp-static');
+var outputPath = __dirname + '/www';
+var port = process.env.PORT || 9000;
+
+harp.compile(__dirname, outputPath, function(errors){
+  if(errors) {
+    console.log(JSON.stringify(errors, null, 2));
+    process.exit(1);
+  }
+
+  console.log('Running harp-static on ' + port);
+  server(outputPath, port);
+});
