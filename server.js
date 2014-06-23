435e1e9b5df6f8dea2b12cda91084a6a6424af0e
Remy Sharp <remy@remysharp.com>
2014-06-23 14:58:57 +0100

    remove redirect...testing.

--- server.js
+++ server.js
@@ -4,14 +4,14 @@ var server = require('harp-static');
 var outputPath = __dirname + '/www';
 var port = process.env.PORT || 9000;
 
-server.middleware.push(function (req, res, next) {
+/*server.middleware.push(function (req, res, next) {
   if (req.headers.host.indexOf('learn.jsbin.com') === 0) {
     res.writeHead(301, { location: 'http://jsbin.com' + req.url });
     res.end();
     return;
   }
   next();
-});
+});*/
 
 harp.compile(__dirname, outputPath, function(errors){
   if(errors) {
