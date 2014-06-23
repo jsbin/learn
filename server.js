d839da6840c8d41f50bffdc7b5a666bae2f4bef0
Remy Sharp <remy@remysharp.com>
2014-06-23 14:43:41 +0100

    Redirect requests to learn.jsbin.com to jsbin.com

--- server.js
+++ server.js
@@ -4,6 +4,15 @@ var server = require('harp-static');
 var outputPath = __dirname + '/www';
 var port = process.env.PORT || 9000;
 
+server.middleware.push(function (req, res, next) {
+  if (req.headers.host.indexOf('learn.jsbin.com') === 0) {
+    res.writeHead(301, { location: 'http://jsbin.com' + req.url });
+    res.end();
+    return;
+  }
+  next();
+});
+
 harp.compile(__dirname, outputPath, function(errors){
   if(errors) {
     console.log(JSON.stringify(errors, null, 2));
