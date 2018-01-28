57c2316bfbcb9b4304baf85cd9f5df5671e6ec32
Remy Sharp <remy@remysharp.com>
2018-01-28 21:50:07 +0000

    feat: adding in search

--- static/js/permalink.js
+++ static/js/permalink.js
@@ -1,11 +1,10 @@
-function permalink(){
+function permalink() {
   'use strict';
-  /*global $*/
   var anchor = document.createElement('a');
   anchor.className = 'anchor';
   anchor.innerHTML = '<span class="permalink"></span>';
 
-  document.querySelectorAll('h1,h2,h3,h4,h5,h6').forEach(function (el) {
+  document.querySelectorAll('h1,h2,h3,h4,h5,h6').forEach(function(el) {
     if (el.id) {
       var clone = anchor.cloneNode(true);
       clone.href = '#' + el.id;
